d600ad9336c0029c6ecf4be647112018d8779200
Joni Ardiansyah <kuronopro@gmail.com>
2022-01-31 14:35:00 +0800

    Hapus combo, tambah colemak, dan pindah layout.

--- keyboards/ocean/slamz/keymaps/kuronopro/keymap.c
+++ keyboards/ocean/slamz/keymaps/kuronopro/keymap.c
@@ -3,9 +3,7 @@
 #include "casemodes.h"
 #include "swapper.h"
 
-enum layers { _QWERTY, _NUMBER, _SYMBOL, _NAVIGATION, _FUNCTION, _MOUSE, _SHORTCUT };
-
-enum combos { COMBO_ESC, COMBO_TAB, COMBO_DEL, COMBO_ENT };
+enum layers { _QWERTY, _COLEMAK, _NUMBER, _SYMBOL, _NAVIGATION, _FUNCTION, _MOUSE, _SHORTCUT };
 
 enum { TD_EQUAL_ARROW, TD_MINUS_ARROW, TD_LT_PHP, TD_GT_PHP };
 
@@ -73,7 +71,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 enum custom_keycodes {
-    NORMAL = SAFE_RANGE,
+    QWERTY = SAFE_RANGE,
+    COLEMAK,
+    NORMAL,
 
     SW_WIN,
 
@@ -92,26 +92,38 @@ enum custom_keycodes {
     VSC_MED
 };
 
-#define R_MOU   LT(_MOUSE, KC_R)
 #define F_NAV   LT(_NAVIGATION, KC_F)
 #define J_SYM   LT(_SYMBOL, KC_J)
-#define Z_SCT   LT(_SHORTCUT, KC_Z)
 #define V_FUN   LT(_FUNCTION, KC_V)
 #define M_NUM   LT(_NUMBER, KC_M)
+#define D_FUN   LT(_FUNCTION, KC_D)
+#define H_NUM   LT(_NUMBER, KC_H)
+#define Q_MOU   LT(_MOUSE, KC_Q)
+#define QUO_SCT LT(_SHORTCUT, KC_QUOT)
 #define MOUSE   TO(_MOUSE)
 
-#define BSP_SFT LSFT_T(KC_BSPC)
-#define SPC_SFT LSFT_T(KC_SPC)
-#define A_GUI   LGUI_T(KC_A)
-#define S_ALT   LALT_T(KC_S)
+#define T_NAV   LT(_NAVIGATION, KC_T)
+#define N_SYM   LT(_SYMBOL, KC_N)
+
 #define D_CTL   LCTL_T(KC_D)
 #define K_CTL   LCTL_T(KC_K)
-#define L_ALT   LALT_T(KC_L)
-#define QUO_GUI LGUI_T(KC_QUOT)
+#define Z_GUI   LGUI_T(KC_Z)
 #define X_ALT   LALT_T(KC_X)
 #define C_CTL   LCTL_T(KC_C)
 #define COM_CTL LCTL_T(KC_COMM)
 #define DOT_ALT LALT_T(KC_DOT)
+#define SLS_GUI LGUI_T(KC_SLSH)
+#define TAB_SFT LSFT_T(KC_TAB)
+#define BSP_SFT LSFT_T(KC_BSPC)
+#define SPC_SFT LSFT_T(KC_SPC)
+#define ENT_SFT LSFT_T(KC_ENT)
+#define N0_SFT  LSFT_T(KC_0)
+#define DOT_SFT LSFT_T(KC_DOT)
+#define GRV_SFT LSFT_T(KC_GRV)
+#define BSL_SFT LSFT_T(KC_BSLS)
+
+#define S_CTL   LCTL_T(KC_S)
+#define E_CTL   LCTL_T(KC_E)
 
 #define EQL_ARR TD(TD_EQUAL_ARROW)
 #define MIN_ARR TD(TD_MINUS_ARROW)
@@ -135,65 +147,60 @@ enum custom_keycodes {
 #define SAVE    C(KC_S)
 #define FIND    C(KC_F)
 
-const uint16_t PROGMEM esc_combo[] = { KC_W, KC_E, COMBO_END };
-const uint16_t PROGMEM tab_combo[] = { S_ALT, D_CTL, COMBO_END };
-const uint16_t PROGMEM del_combo[] = { KC_O, KC_I, COMBO_END };
-const uint16_t PROGMEM ent_combo[] = { L_ALT, K_CTL, COMBO_END };
-
-combo_t key_combos[COMBO_COUNT] = {
-    [COMBO_ESC] = COMBO(esc_combo, KC_ESC),
-    [COMBO_TAB] = COMBO(tab_combo, KC_TAB),
-    [COMBO_DEL] = COMBO(del_combo, KC_DEL),
-    [COMBO_ENT] = COMBO(ent_combo, KC_ENT),
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    R_MOU,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        A_GUI,   S_ALT,   D_CTL,   F_NAV,   KC_G,    KC_H,    J_SYM,   K_CTL,   L_ALT,   QUO_GUI,
-        Z_SCT,   X_ALT,   C_CTL,   V_FUN,   KC_B,    KC_N,    M_NUM,   COM_CTL, DOT_ALT, KC_SLSH,
-        XXXXXXX, XXXXXXX, KC_TAB,  XXXXXXX, BSP_SFT, SPC_SFT, XXXXXXX, KC_ENT,  XXXXXXX, XXXXXXX
+        Q_MOU,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    D_CTL,   F_NAV,   KC_G,    KC_H,    J_SYM,   K_CTL,   KC_L,    QUO_SCT,
+        Z_GUI,   X_ALT,   C_CTL,   V_FUN,   KC_B,    KC_N,    M_NUM,   COM_CTL, DOT_ALT, SLS_GUI,
+        QWERTY,  XXXXXXX, KC_ESC,  TAB_SFT, BSP_SFT, SPC_SFT, SPC_SFT, KC_ENT,  XXXXXXX, COLEMAK
+    ),
+
+    [_COLEMAK] = LAYOUT(
+        Q_MOU,   KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    QUO_SCT,
+        KC_A,    KC_R,    S_CTL,   T_NAV,   KC_G,    KC_M,    N_SYM,   E_CTL,   KC_I,    KC_O,
+        Z_GUI,   X_ALT,   C_CTL,   D_FUN,   KC_V,    KC_K,    H_NUM,   COM_CTL, DOT_ALT, SLS_GUI,
+        QWERTY,  XXXXXXX, KC_ESC,  TAB_SFT, BSP_SFT, SPC_SFT, SPC_SFT, KC_ENT,  XXXXXXX, COLEMAK
     ),
 
     [_NUMBER] = LAYOUT(
-        KC_GRV,  KC_7,    KC_8,    KC_9,    KC_BSLS, _______, _______, KC_LCTL, KC_LALT, KC_LGUI,
-        KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS, _______, _______, _______, KC_MINS, KC_SLSH,
-        KC_0,    KC_1,    KC_2,    KC_3,    KC_DOT,  _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        KC_SLSH, KC_7,    KC_8,    KC_9,    KC_MINS, _______, _______, _______, _______, _______,
+        KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS, _______, _______, _______, _______, _______,
+        KC_COLN, KC_1,    KC_2,    KC_3,    KC_EQL,  _______, _______, KC_LCTL, KC_LALT, KC_LGUI,
+        _______, _______, _______, DOT_SFT, N0_SFT,  _______, _______, _______, _______, _______
     ),
 
     [_SYMBOL] = LAYOUT(
-        KC_LPRN, KC_LBRC, KC_COLN, KC_RBRC, KC_RPRN, _______, _______, _______, _______, _______,
-        KC_DLR,  KC_DQUO, MIN_ARR, KC_UNDS, KC_SCLN, _______, _______, KC_LCTL, KC_LALT, KC_LGUI,
-        KC_LCBR, LT_PHP,  EQL_ARR, GT_PHP,  KC_RCBR, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        KC_LPRN, KC_LBRC, KC_COLN, KC_RBRC, KC_RPRN, _______, _______, KC_AMPR, KC_EXLM, KC_CIRC,
+        KC_DLR,  KC_DQUO, MIN_ARR, KC_UNDS, KC_SCLN, _______, _______, KC_LCTL, KC_AT,   KC_HASH,
+        KC_LCBR, LT_PHP,  EQL_ARR, GT_PHP,  KC_RCBR, _______, _______, KC_PERC, KC_LALT, KC_LGUI,
+        _______, _______, _______, GRV_SFT, BSL_SFT, _______, _______, _______, _______, _______
     ),
 
     [_NAVIGATION] = LAYOUT(
-        _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_ESC,
-        KC_LGUI, KC_LALT, KC_LCTL, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, SW_WIN,
-        _______, _______, _______, _______, _______, WSM_LFT, WS_LEFT, WS_RGHT, WSM_RGT, KC_DEL,
+        KC_ESC,  _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_ESC,
+        _______, _______, KC_LCTL, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, SW_WIN,
+        KC_LGUI, KC_LALT, _______, _______, _______, WSM_LFT, WS_LEFT, WS_RGHT, WSM_RGT, KC_DEL,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     [_FUNCTION] = LAYOUT(
-        _______, _______, _______, _______, _______, KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,
+        _______, _______, _______, _______, _______, KC_PSCR, KC_F9,   KC_F10,  KC_F11,  KC_F12,
         _______, _______, _______, _______, _______, KC_INS,  KC_F5,   KC_F6,   KC_F7,   KC_F8,
-        KC_LGUI, KC_LALT, KC_LCTL, _______, _______, KC_CAPS, KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_LGUI, KC_LALT, KC_LCTL, _______, _______, KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     [_MOUSE] = LAYOUT(
-        KC_LGUI, KC_LALT, KC_LCTL, _______, _______, KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, KC_ESC,
+        KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______, KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, KC_ESC,
         SEL_ALL, SAVE,    _______, FIND,    _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, SW_WIN,
         UNDO,    CUT,     COPY,    PASTE,   REDO,    _______, _______, _______, _______, NORMAL,
-        _______, _______, KC_BTN2, _______, KC_BTN1, KC_BTN1, _______, KC_BTN2, _______, _______
+        _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_BTN1, _______, KC_BTN2, _______, _______
     ),
 
     [_SHORTCUT] = LAYOUT(
-        RESET,   KC_MUTE, KC_MNXT, KC_VOLU, KC_BRIU, VSC_NEW, KC_F12,  VSC_BAK, _______, VSC_CLA,
+        RESET,   KC_MUTE, KC_MNXT, KC_VOLU, KC_BRIU, VSC_NEW, KC_F12,  VSC_BAK, VSC_CLA, _______,
         _______, _______, KC_MPLY, KC_VOLD, KC_BRID, VSC_FEL, VSC_FED, VSC_FEU, VSC_FER, _______,
-        _______, _______, ANYCASE, CAPSWRD, SNKCASE, VSC_MEL, VSC_MED, VSC_MEU, VSC_MER, MOUSE,
+        _______, ANYCASE, SNKCASE, CAPSWRD, _______, VSC_MEL, VSC_MED, VSC_MEU, VSC_MER, MOUSE,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
@@ -207,6 +214,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     );
 
     switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return false;
+
+        case COLEMAK:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_COLEMAK);
+            }
+            return false;
+
         case NORMAL:
             layer_clear();
             return false;
@@ -329,12 +348,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case S_ALT:
-        case SPC_SFT:
-        case BSP_SFT:
-            return TAPPING_TERM + 15;
-        case A_GUI:
-            return TAPPING_TERM + 25;
+        case F_NAV:
+        case J_SYM:
+        case T_NAV:
+        case N_SYM:
+            return TAPPING_TERM - 50;
         default:
             return TAPPING_TERM;
     }
@@ -342,10 +360,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case A_GUI:
-        case S_ALT:
-        case R_MOU:
-            return false;
         default:
             return true;
     }
--- keyboards/ocean/slamz/keymaps/kuronopro/rules.mk
+++ keyboards/ocean/slamz/keymaps/kuronopro/rules.mk
@@ -5,4 +5,3 @@ SRC += oneshot.c
 MOUSEKEY_ENABLE = yes # Mouse keys
 EXTRAKEY_ENABLE = yes # Audio control and System control
 TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = yes
