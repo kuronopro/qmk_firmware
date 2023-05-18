a2e5c4a69cffcd0623972e9948cfaa5d20c4aa53
Joni Ardiansyah <kuronopro@gmail.com>
2023-05-18 15:35:27 +0800

    Update keymap

--- keyboards/ocean/slamz/keymaps/kuronopro/keymap.c
+++ keyboards/ocean/slamz/keymaps/kuronopro/keymap.c
@@ -19,7 +19,7 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 #define CAPSWRD CW_TOGG
-#define RESET   QK_RBT
+#define RESET   QK_BOOT
 
 #define LT_PHP  TD(TD_LT_PHP)
 #define GT_PHP  TD(TD_GT_PHP)
@@ -49,47 +49,52 @@ tap_dance_action_t tap_dance_actions[] = {
 #define J_SYM   LT(_SYMBOL, KC_J)
 #define MOUSE   TO(_MOUSE)
 
+#define TO_SYM  TO(_SYMBOL)
+#define TO_NUM  TO(_NUMBER)
+#define TO_NAV  TO(_NAVIGATION)
+#define TO_FUN  TO(_FUNCTION)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_kuronopro(
         Q_MOUS,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         A_CTL,   S_OPT,   D_CMD,   F_NAV,   G_NUM,   H_FUN,   J_SYM,   K_CMD,   L_OPT,   QUO_CTL,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   KC_TAB,  BSP_SFT, SPC_SFT, KC_ENT
+        TO_SYM,  TO_FUN,           KC_TAB,  BSP_SFT, SPC_SFT, KC_ENT,           TO_NAV,  TO_NUM
     ),
 
     [_SYMBOL] = LAYOUT_kuronopro(
         KC_LPRN, KC_LBRC, KC_COLN, KC_RBRC, KC_RPRN, _______, KC_CIRC, KC_EXLM, KC_AT,   KC_HASH,
         KC_DLR,  KC_DQUO, KC_MINS, KC_UNDS, KC_SCLN, _______, _______, KC_RCMD, KC_LOPT, KC_RCTL,
         KC_LCBR, LT_PHP,  KC_EQL,  GT_PHP,  KC_RCBR, _______, _______, KC_AMPR, KC_PERC, KC_QUES,
-                                   KC_GRV,  KC_BSLS, _______, _______
+        NORMAL,  _______,          KC_GRV,  KC_BSLS, _______, _______,          _______, _______
     ),
 
     [_NUMBER] = LAYOUT_kuronopro(
         _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_COLN, KC_EQL,
         KC_LCTL, KC_LOPT, KC_LCMD, _______, _______, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_ASTR,
         _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_MINS, KC_SLSH,
-                                   _______, _______, KC_0,    KC_DOT
+        _______, _______,          _______, _______, KC_0,    KC_DOT,           _______, NORMAL
     ),
 
     [_NAVIGATION] = LAYOUT_kuronopro(
         KC_ESC,  TAB_LFT, TAB_RGT, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______,
         KC_LCTL, KC_LOPT, KC_LCMD, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DEL,
         SW_WIN,  BACK,    FWD,     _______, _______, _______, _______, _______, _______, MOUSE,
-                                   _______, _______, _______, _______
+        _______, _______,          _______, _______, _______, _______,          NORMAL,  _______
     ),
 
     [_FUNCTION] = LAYOUT_kuronopro(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, CAPSWRD, SNKCASE, KBBCASE,
         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, KC_RCMD, KC_LOPT, KC_RCTL,
         KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  _______, _______, _______, _______, RESET,
-                                   _______, KC_F19,  _______, _______
+        _______, NORMAL,           _______, KC_F19,  _______, _______,          _______, _______
     ),
 
     [_MOUSE] = LAYOUT_kuronopro(
         _______, SPC_LFT, APPWNDW, MSNCTRL, SPC_RGT, KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, _______,
         _______, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
         _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, NORMAL,
-                                   KC_BTN2, KC_BTN1, KC_BTN1, KC_BTN2
+        _______, _______,          KC_BTN2, KC_BTN1, KC_BTN1, KC_BTN2,          _______, _______
     ),
 };
 
@@ -185,7 +190,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 enum combos {
     WE_ESC,
     MCOM_MINS,
-    COMDOT_SCLN,
+    COMDOT_SCLN
 };
 
 const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
